83f8cd25036baeded70cd02f6454504b3a8c2619
Oliver Lunding Sandqvist <oliver.sandqvist@outlook.dk>
2020-05-31 14:02:27 +0200

    Implementation of additional probability and intensity NUnit-tests

--- NUnitTests/UnitTestProbabilities.cs
+++ NUnitTests/UnitTestProbabilities.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using ProjectionSemiMarkov;
 using static ProjectionSemiMarkov.HelperFunctions;
 using static ProjectionSemiMarkov.Setup;
+using static ProjectionSemiMarkov.ProbabilityCalculator;
 
 namespace NUnitTests
 {
@@ -33,8 +34,13 @@ namespace NUnitTests
       epsilon = Math.Pow(10, -15);
       marketBasis = CreateMarketBasisIntensities();
       technicalBasis = CreateTechnicalBasisIntensities();
-      stateSpace = new [] { State.Active,State.Disabled,State.Dead,State.Surrender,State.FreePolicyActive,
-        State.FreePolicyDisabled,State.FreePolicyDead,State.FreePolicySurrender, };
+
+      var allPossibleTransitions = marketBasis[Gender.Female].Union(marketBasis[Gender.Male]).ToList();
+      stateSpace = allPossibleTransitions.SelectMany(x => x.Value.Keys)
+      .Union(allPossibleTransitions.Select(y => y.Key)).Distinct();
+
+      //stateSpace = new [] { State.Active,State.Disabled,State.Dead,State.Surrender,State.FreePolicyActive,
+      //  State.FreePolicyDisabled,State.FreePolicyDead,State.FreePolicySurrender, };
     }
 
     [Test]
@@ -51,10 +57,87 @@ namespace NUnitTests
     }
 
     [Test]
-    public void Test2()
+    public void FirstFiveProbabilitiesAtUmaxAtActiveAreUnchanged()
     {
-      var x = -1;
-      Assert.Negative(x);
+      var marketProbabilityCalculator = new ProbabilityCalculator(marketBasis, policies);
+      var probabilities = marketProbabilityCalculator.Calculate();
+
+      var targetProbabilities = new List<double>();
+      targetProbabilities.Add(1.0); 
+      targetProbabilities.Add(0.98893149449659778);
+      targetProbabilities.Add(0.97799283951155114);
+      targetProbabilities.Add(0.96718243333451615);
+      targetProbabilities.Add(0.95649869488622441);
+
+      for (var t = 0; t < 5; t++)
+      {
+        int umax = marketProbabilityCalculator.DurationSupportIndex(policy1.initialDuration,t);
+        Assert.That(probabilities[policy1.policyId][State.Active][t][umax], Is.EqualTo(targetProbabilities[t]).Within(epsilon));
+      }
     }
+
+    [Test]
+    public void ProbabilitiesAtActiveAreDurationDependent()
+    {
+      var marketProbabilityCalculator = new ProbabilityCalculator(marketBasis, policies);
+      var probabilities = marketProbabilityCalculator.Calculate();
+
+      for (var t = 1; t < marketProbabilityCalculator.GetNumberOfTimePoints(policy1); t++)
+      {
+        var probabilityList = probabilities[policy1.policyId][State.Active][t].Distinct();
+        var m = 0;
+        foreach(var e in probabilities[policy1.policyId][State.Active][t].Distinct())
+        {
+          m=m+1;
+        }
+        Assert.That(m,Is.Not.EqualTo(1));
+      }
+    }
+
+    [Test]
+    public void IntensitiesAreNotTooLarge()
+    {
+      var marketProbabilityCalculator = new ProbabilityCalculator(marketBasis, policies);
+
+      foreach (var g in Enum.GetValues(typeof(Gender)).Cast<Gender>())
+      {
+         var genderIntensity = marketBasis[g];
+        foreach (var j in genderIntensity.Keys)
+        {
+          foreach (var i in genderIntensity[j].Keys)
+          {
+            for (var t = 0; t <= policy1.expiryAge; t++)
+            {
+              int umax = marketProbabilityCalculator.DurationSupportIndex(policy1.initialDuration, t);
+              for (var u = 0; u <= umax; u++)
+              {
+                Assert.That(marketBasis[g][j][i](t, u), Is.LessThanOrEqualTo(10));
+              }
+            }
+          }
+        }
+      }
+    }
+
+    [Test]
+    public void MuDotForActiveIsSumOfIntensities()
+    {
+      var marketProbabilityCalculator = new ProbabilityCalculator(marketBasis, policies);
+
+      for (var t = 0; t <= policy1.expiryAge; t++)
+            {
+              int umax = marketProbabilityCalculator.DurationSupportIndex(policy1.initialDuration, t);
+              for (var u = 0; u <= umax; u++)
+              {
+                Assert.That(marketBasis[policy1.gender][State.Active][State.Active](t, u), Is.EqualTo(
+                  marketBasis[policy1.gender][State.Active][State.Disabled](t, u) + 
+                  marketBasis[policy1.gender][State.Active][State.FreePolicyActive](t, u) +
+                  marketBasis[policy1.gender][State.Active][State.Dead](t, u) +
+                  marketBasis[policy1.gender][State.Active][State.Surrender](t, u)
+                  ).Within(epsilon));
+              }
+            }
+    }
+
   }
 }
